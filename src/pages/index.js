18987ec92287f7d352aaf9f1da4d1c5c9cf1bf99
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-06 16:27:40 +0100

    remove unused code

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,5 @@ import React from "react"
 import Layout from "../components/Layout.js"
 
 export default () => {
-  return (
-    <Layout>
-      <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around"></main>
-    </Layout>
-  )
+  return <Layout></Layout>
 }
