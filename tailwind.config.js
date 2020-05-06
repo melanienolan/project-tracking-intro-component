c3585151684d8e4747b2feba54403214d4d8c6a2
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-06 16:25:31 +0100

    extend theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ["./src/**/*.js"],
   target: "relaxed",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "hsl(0, 100%, 68%)",
+        neutral: {
+          "100": "hsl(207, 33%, 95%)",
+          "200": "hsl(230, 11%, 40%)",
+          "300": "hsl(230, 29%, 20%)",
+          "400": "hsl(231, 7%, 65%)",
+        },
+      },
+      fontFamily: {
+        barlow: ["Barlow", "sans-serif"],
+        condensed: ["Barlow Condensed", "sans-serif"],
+      },
+    },
   },
   variants: {},
   plugins: [],
