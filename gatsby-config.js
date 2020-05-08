3f3afba0f6a374080f6411bd544a790bf1023eac
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-08 15:03:51 +0100

    update project details

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,17 +5,17 @@ const { theme } = resolveConfig(tailwindConfig)
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter Tailwind CSS",
+    title: "Project Tracking",
     author: "Melanie Nolan",
-    description: "A Gatsby starter to set you up with Tailwind CSS",
+    description: "",
   },
   plugins: [
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        name: "Gatsby Starter Tailwind CSS",
-        short_name: "Gatsby Starter Tailwind CSS",
+        name: "Project Tracking Awesome",
+        short_name: "Project Tracking Awesome",
         start_url: "/",
         background_color: theme.colors.white,
         theme_color: theme.colors.primary,
