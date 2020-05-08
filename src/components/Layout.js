f476cfe576b8ef5f754918c0631ba62a579f5c7e
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-08 13:05:03 +0100

    move decoration to layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,6 +5,14 @@ const Layout = ({ children }) => {
   return (
     <>
       <SEO />
+      <div
+        className="absolute top-0 right-0 w-1/2 bg-neutral-100"
+        style={{
+          height: "45vh",
+          borderBottomLeftRadius: "6rem",
+          zIndex: "-1",
+        }}
+      ></div>
       <div className="container mx-auto h-screen">{children}</div>
     </>
   )
