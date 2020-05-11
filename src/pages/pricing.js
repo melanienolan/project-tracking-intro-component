871fd502272283cbaf5c23e5f0d4e315c2340d2d
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-11 16:39:11 +0100

    create pages for links

---
+++ src/pages/pricing.js
@@ -0,0 +1,16 @@
+import React from "react"
+import Layout from "../components/Layout.js"
+
+export default () => {
+  return (
+    <Layout>
+      <main className="sm:mt-16">
+        <div className="mt-24 ml-8 sm:mt-8">
+          <h2 className="mt-4 font-condensed font-bold text-neutral-300 leading-none uppercase text-3xl max-w-xs sm:max-w-none sm:text-4xl">
+            Pricing
+          </h2>
+        </div>
+      </main>
+    </Layout>
+  )
+}
