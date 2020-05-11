c4f2b8cf8519b0f9a98c5bcb53b8a197175518ee
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-11 16:46:09 +0100

    testing something

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,1 @@
 require("./src/css/tailwind.css")
-
-exports.onClientEntry = () => {
-  window.addEventListener("load", () => {
-    document.body.className = document.body.className.replace(/\bno-js\b/, "")
-  })
-}
