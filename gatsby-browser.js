eb57002d8bc26856b9949e1d617ae482d5b504b6
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-11 16:52:41 +0100

    Revert "testing something"
    This reverts commit c4f2b8cf8519b0f9a98c5bcb53b8a197175518ee.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,7 @@
 require("./src/css/tailwind.css")
+
+exports.onClientEntry = () => {
+  window.addEventListener("load", () => {
+    document.body.className = document.body.className.replace(/\bno-js\b/, "")
+  })
+}
