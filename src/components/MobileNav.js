ad7c8d35348ad8a1fc7ca8461eaf62f77d3e238b
Melanie Nolan <melanie.nolan@yahoo.co.uk>
2020-05-21 17:04:50 +0100

    use activeClassName for nav items

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -6,16 +6,24 @@ const MobileNav = () => {
     <nav className="p-4 absolute inset-x-2 font-condensed font-bold text-neutral-300 bg-white shadow-xl rounded uppercase sm:hidden">
       <ul className="flex flex-col list-none text-center">
         <li className="mt-5 hover:underline">
-          <Link to="/product">Product</Link>
+          <Link to="/product" activeClassName="underline">
+            Product
+          </Link>
         </li>
         <li className="mt-5 hover:underline">
-          <Link to="/features">Features</Link>
+          <Link to="/features" activeClassName="underline">
+            Features
+          </Link>
         </li>
         <li className="mt-5 hover:underline">
-          <Link to="/pricing">Pricing</Link>
+          <Link to="/pricing" activeClassName="underline">
+            Pricing
+          </Link>
         </li>
         <li className="mt-3 pt-2 text-neutral-400 border-t hover:underline">
-          <Link to="/login">Login</Link>
+          <Link to="/login" activeClassName="underline">
+            Login
+          </Link>
         </li>
       </ul>
     </nav>
--- src/components/NormalNav.js
+++ src/components/NormalNav.js
@@ -6,17 +6,25 @@ const NormalNav = () => {
     <nav className="hidden sm:block font-condensed font-bold text-neutral-300 uppercase">
       <ul className="list-none sm:flex sm:justify-between sm:items-center">
         <li className="mr-5 hover:underline">
-          <Link to="/product">Product</Link>
+          <Link to="/product" activeClassName="underline">
+            Product
+          </Link>
         </li>
         <li className="mx-5 hover:underline">
-          <Link to="/features">Features</Link>
+          <Link to="/features" activeClassName="underline">
+            Features
+          </Link>
         </li>
         <li className="mx-5 hover:underline">
-          <Link to="/pricing">Pricing</Link>
+          <Link to="/pricing" activeClassName="underline">
+            Pricing
+          </Link>
         </li>
         <li className="mx-5 text-neutral-400">&bull;</li>
         <li className="ml-5 text-neutral-400 hover:underline">
-          <Link to="/login">Login</Link>
+          <Link to="/login" activeClassName="underline">
+            Login
+          </Link>
         </li>
       </ul>
     </nav>
